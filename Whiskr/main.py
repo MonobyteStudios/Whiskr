cacab134d4d457db13f4e6326733db5796ccf2f5
🅱️inode <111764732+As2Bax@users.noreply.github.com>
2025-05-05 00:24:55 -0500

    Update main.py

--- Whiskr/main.py
+++ Whiskr/main.py
@@ -3,9 +3,6 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 
-print("Startup request recieved - Bot booting up..")
-print("Whiskr (Open Sourced) - Developed by vqel")
-
 intents = discord.Intents.default()
 intents.message_content = True
 
