643886fce0c40f1bc0de0e5fd2030a0923e85415
🅱️inode <111764732+As2Bax@users.noreply.github.com>
2025-05-04 19:42:17 -0500

    adding files (again)

---
+++ main.py
@@ -0,0 +1,43 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+print("Startup request recieved - Bot booting up..")
+print("Whiskr (Open Sourced) - Developed by vqel")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = commands.Bot(
+    command_prefix="!", 
+    intents=intents
+)
+
+@client.event
+async def on_ready():
+    print("Loading extensions...")
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py") and not filename.startswith("_"):
+            try:
+                extension = f"cogs.{filename[:-3]}"
+                await client.load_extension(extension)
+                print(f"Loaded extension: {extension}")
+
+            except Exception as e:
+                print(f"Failed to load extension {extension}: {e}")
+
+    print(f"Successfully logged in as {client.user}!")
+
+    print("Syncing slash commands..")
+    try:
+        synced = await client.tree.sync()
+        print(f"Successfully synced {len(synced)} slash commands!")
+
+    except Exception as e:
+        print(f"An error has occured while attempting to sync slash commands: **{e}**")
+
+
+load_dotenv(dotenv_path=os.path.join("config.env"))
+TOKEN = os.getenv("DISCORD_TOKEN")
+client.run(TOKEN)
